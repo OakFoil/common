dc41b3a67a5a307cdcdf8baffbe0322688735719
OakFoil <elsennahway@gmail.com>
2026-07-12 07:45:59 +0300

    rename package to common_flutter

--- lib/src/components/async.dart
+++ lib/src/components/async.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:common/common.dart';
+import 'package:common_flutter/common.dart';
 import 'package:riverpod/riverpod.dart';
 
 class MkFutureBuilder<T> extends StatelessWidget {
